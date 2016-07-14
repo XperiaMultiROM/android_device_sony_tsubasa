2e7430559137269169963980d68c0e997661e5b0
Adrian DC <radian.dc@gmail.com>
2016-07-15 01:23:00 +0200

    tsubasa: Disable touchscreen inputs on purpose
     * For the time being, Xperia Blue RMI4 touchscreens
        are registering permament presses preventing
        proper usage of the touchscreen in MultiROM UI
    
    Change-Id: I52a4a7c4942961b855a350fd420c242f0ffb1fb2

--- multirom/mr_init_devices.c
+++ multirom/mr_init_devices.c
@@ -20,12 +20,10 @@ const char *mr_init_devices[] =
     // Inputs
     "/sys/devices/virtual/input*",
     "/sys/devices/virtual/misc/uinput",
-    "/sys/devices/i2c-3/3-002c/sensor00/sensor00.f11/input/input*",
     "/sys/devices/platform/msm_ssbi.0/pm8921-core/pm8xxx-keypad/input*",
     "/sys/devices/platform/msm_ssbi.0/pm8921-core/pm8xxx-pwrkey/input*",
     "/sys/devices/platform/gpio-keys.0/input/input*",
     "/sys/class/input/event0",
-    "/sys/class/input/event7",
     "/sys/class/input/event8",
 
     // ADB
