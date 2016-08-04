0e17b2c4f0da1ca5ba93f867dac3ea562b55c940
Adrian DC <radian.dc@gmail.com>
2016-08-05 01:23:21 +0200

    tsubasa: multirom: Update the inputs inits
     * Delete the hardcoded event numbers
     * Use only specific input path of each drivers' inputs
    
    Change-Id: Icb0cf428494534e177c74957fea1aaafe891d69c

--- multirom/mr_init_devices.c
+++ multirom/mr_init_devices.c
@@ -18,13 +18,12 @@ const char *mr_init_devices[] =
     "/sys/devices/virtual/misc/fuse",
 
     // Inputs
-    "/sys/devices/virtual/input*",
-    "/sys/devices/virtual/misc/uinput",
+    "/sys/devices/i2c-3/3-002c/sensor00/sensor00.f11/input*",
+    "/sys/devices/platform/gpio-keys.0/input*",
     "/sys/devices/platform/msm_ssbi.0/pm8921-core/pm8xxx-keypad/input*",
     "/sys/devices/platform/msm_ssbi.0/pm8921-core/pm8xxx-pwrkey/input*",
-    "/sys/devices/platform/gpio-keys.0/input/input*",
-    "/sys/class/input/event0",
-    "/sys/class/input/event8",
+    "/sys/devices/virtual/input*",
+    "/sys/devices/virtual/misc/uinput",
 
     // ADB
     "/sys/class/tty/ptmx",
